f0c43ac22b268cb89124188325516c9a29e4f516
Luiz Cláudio <luizcns18@gmail.com>
2020-06-19 08:34:21 -0300

    Feature/stack data structure (#2)
    * Add stack and base converter example
    
    * Improve files organization
    
    * Export algorithms and others

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+//Data structure
+export { default as Stack } from "./data-structures/stack";
+
+//Algorithms
+
+//Others
+export { decimalToBinary } from "./others/base-converter";
+export { baseConverter } from "./others/base-converter";
