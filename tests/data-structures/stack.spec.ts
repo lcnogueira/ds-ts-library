149b2012a97e026121325466dfa47a2cebfd5785
Luiz Cláudio <luizcns18@gmail.com>
2020-06-25 20:44:06 -0300

    Refactor stack tests (#14)

--- tests/data-structures/stack.spec.ts
+++ tests/data-structures/stack.spec.ts
@@ -7,51 +7,51 @@ describe('Stack', () => {
     stack = new Stack();
   });
 
-  it('should start empty', () => {
-    expect(stack.size()).toEqual(0);
-    expect(stack.isEmpty()).toEqual(true);
+  it('starts empty', () => {
+    expect(stack.size()).toBe(0);
+    expect(stack.isEmpty()).toBe(true);
   });
 
-  it('should push elements to the end', () => {
+  it('pushes elements to the end', () => {
     stack.push(1);
     stack.push(2);
-    expect(stack.size()).toEqual(2);
-    expect(stack.peek()).toEqual(2);
+    expect(stack.size()).toBe(2);
+    expect(stack.peek()).toBe(2);
   });
 
-  it('should pop elements from the end', () => {
+  it('pops elements from the end', () => {
     stack.push(1);
     stack.push(2);
     stack.push(3);
-    expect(stack.pop()).toEqual(3);
-    expect(stack.size()).toEqual(2);
-    expect(stack.peek()).toEqual(2);
+    expect(stack.pop()).toBe(3);
+    expect(stack.size()).toBe(2);
+    expect(stack.peek()).toBe(2);
   });
 
-  it('should return undefined when trying to pop from an empty stack', () => {
+  it('returns undefined when trying to pop from an empty stack', () => {
     expect(stack.isEmpty()).toBeTruthy();
     expect(stack.pop()).toBeUndefined();
   });
 
-  it('should peek at the top element in the stack without poping it', () => {
+  it('peeks at the top element in the stack without poping it', () => {
     stack.push(1);
     stack.push(2);
-    expect(stack.peek()).toEqual(2);
+    expect(stack.peek()).toBe(2);
     expect(stack.size()).toBe(2);
   });
 
-  it('should return undefined when trying to peek from an empty stack', () => {
+  it('returns undefined when trying to peek from an empty stack', () => {
     expect(stack.isEmpty()).toBeTruthy();
     expect(stack.peek()).toBeUndefined();
   });
 
-  it('should return the correct size if size() is called', () => {
+  it('returns the correct size if size() is called', () => {
     expect(stack.isEmpty()).toBeTruthy();
     stack.push(1);
-    expect(stack.size()).toEqual(1);
+    expect(stack.size()).toBe(1);
   });
 
-  it('should clear the stack if clear() is called', () => {
+  it('clears the stack if clear() is called', () => {
     stack.clear();
     expect(stack.isEmpty()).toBeTruthy();
 
@@ -62,22 +62,22 @@ describe('Stack', () => {
     expect(stack.isEmpty()).toBeTruthy();
   });
 
-  it('should return a comma separated string if toString() is called', () => {
+  it('returns a comma separated string if toString() is called', () => {
     expect(stack.isEmpty()).toBeTruthy();
     stack.push(1);
-    expect(stack.toString()).toEqual('1');
+    expect(stack.toString()).toBe('1');
     stack.push(2);
-    expect(stack.toString()).toEqual('1,2');
+    expect(stack.toString()).toBe('1,2');
 
     const stackString = new Stack<string>();
     stackString.push('el1');
-    expect(stackString.toString()).toEqual('el1');
+    expect(stackString.toString()).toBe('el1');
     stackString.push('el2');
-    expect(stackString.toString()).toEqual('el1,el2');
+    expect(stackString.toString()).toBe('el1,el2');
   });
 
-  it('should return an empty string if toString() is called and the stack is empty', () => {
+  it('returns an empty string if toString() is called and the stack is empty', () => {
     expect(stack.isEmpty()).toBeTruthy();
-    expect(stack.toString()).toEqual('');
+    expect(stack.toString()).toBe('');
   });
 });
